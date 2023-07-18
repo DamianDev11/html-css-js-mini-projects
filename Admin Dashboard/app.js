933b0a5334c5442f7a36d2c8683857366b9004ee
Damian <damianprakash07@gmail.com>
2023-07-18 23:41:41 +0530

    fixed dark theme toggle

--- Admin Dashboard/app.js
+++ Admin Dashboard/app.js
@@ -48,3 +48,13 @@ window.addEventListener("resize", () => {
     searchForm.classList.remove("show");
   }
 });
+
+const toggle = document.getElementById("theme-toggle");
+
+toggle.addEventListener("change", function () {
+  if (this.checked) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+});
